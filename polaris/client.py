712aee148e6b62e8855ffb1c9ddcbcd23efc8cf3
Mattia Procopio <promat85@gmail.com>
2020-03-19 09:42:58 +0100

    Improve PDF scraping

--- polaris/client.py
+++ polaris/client.py
@@ -1,38 +1,50 @@
 import csv
 import datetime
+import re
 from datetime import datetime as dt
 
 import tabula
 
 REPORT_1_DATE = '2020-01-21'
 WHO_URL = 'https://www.who.int/docs/default-source/coronaviruse/situation-reports/'
+EXTRACT_NUMBER_FROM_COUNTRY_REGEX = re.compile(r'^([\w\s\)]+) (\d+)$', re.UNICODE)
 
 
 class Polaris:
 
-    def _get_yesterday_pdf_url(self, today_datetime: datetime.datetime) -> str:
-        yesterday_report_number = (today_datetime - dt.strptime(REPORT_1_DATE, '%Y-%m-%d')).days
-        return f'{WHO_URL}{(today_datetime - datetime.timedelta(days=1)).date().isoformat().replace("-", "")}-sitrep-{yesterday_report_number}-covid-19.pdf'
+    def _get_pdf_url(self, day: datetime.datetime) -> str:
+        report_number = (day - dt.strptime(REPORT_1_DATE, '%Y-%m-%d')).days + 1
+        return f'{WHO_URL}{day.date().isoformat().replace("-", "")}-sitrep-{report_number}-covid-19.pdf'
 
     def _is_interesting_row(self, row: list) -> bool:
         try:
-            if row[1] in ('', None):
+            if not row[5] or not row[6]:
                 return False
-
-            try:
-                int(row[1])
+            else:
                 return True
-            except ValueError:
-                return False
 
         except IndexError:
             return False
 
     def _sanitize_row(self, row: list) -> list:
-        if not row[2]:
-            row.pop(2)
+        new_row = []
+        country = None
+        try:
+            int(row[0][-1:])
+            if match := EXTRACT_NUMBER_FROM_COUNTRY_REGEX.search(row[0]):  # NOQA
+                try:
+                    country = match.group(1)
+                except AttributeError:
+                    pass
+                total_cases = match.group(2)
+        except ValueError:
+            # No number no party
+            pass
 
-        return row
+        if country:
+            return [country, total_cases, row[2], row[3], row[4]]
+        else:
+            return [row[0], row[1], row[2], row[3], row[4]]
 
     def _clean_csv(self, name: str) -> None:
         new_lines = []
@@ -41,17 +53,19 @@ class Polaris:
             writer = csv.writer(csvfile, quotechar='|')
             for row in reader:
                 if self._is_interesting_row(row):
-                    self._sanitize_row(row)
-                    new_lines.append(row)
+                    new_row = self._sanitize_row(row)
+                    print('ROW', row)
+                    new_row.append(name)
+                    new_lines.append(new_row)
 
             # Reset the file
             csvfile.seek(0)
             writer.writerows(new_lines)
             csvfile.truncate()
 
-    def get_latest_csv_data(self) -> None:
-        today_datetime = dt.today()
-        today = dt.date(today_datetime).isoformat()
-        url = self._get_yesterday_pdf_url(today_datetime)
-        tabula.convert_into(url, f'{today}.csv', output_format='csv', pages='all')
-        self._clean_csv(today)
+    def get_data(self, date: str, page: list = [3, 4, 5, 6]) -> None:
+        day = dt.strptime(date, '%Y-%m-%d')
+        url = self._get_pdf_url(day)
+        print('PDF URL:', url)
+        tabula.convert_into(url, f'{day.date()}.csv', output_format='csv', pages=page)
+        self._clean_csv(str(day.date()))
